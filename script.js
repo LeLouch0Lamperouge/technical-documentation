718f68687f974d6cd3686200413fb6de2d045f2d
Hassan El-Deghedy <106387607+LoLoch0@users.noreply.github.com>
2022-05-28 21:59:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,7 @@
 
 document.addEventListener("click", function(event) {
   if (event.target.matches("#menuOpen") ) {
-      document.getElementById("sidebar").classList.remove("close");
-      document.getElementById("menuOpen").style.display = "none";
-      document.getElementById("menuClose").style.display = "block";
-
-      
-      const mediaQuery = window.matchMedia('(max-width: 400px)')
-      if (mediaQuery.matches) {
-        const boxes = document.querySelectorAll('.sectors');
-        for (const sectors of boxes) {
-          sectors.style.width = "calc(100% - 260px)";
-        }
-        const flexo = document.querySelectorAll('.atract');
-        for (const atract of flexo) {
-          atract.style.display = "flex";
-        }
-        const widthi = document.querySelectorAll('.sectors');
-        for (const sectors of widthi) {
-          sectors.style.width = "calc(100% - 78px)";
-        }
-      }
-
+      document.getElementById("sidebar").classList.remove("transform");
   }
 },
 false
@@ -29,25 +9,7 @@ false
 
 document.addEventListener("click", function(event) {
   if (event.target.matches("#menuClose") ) {
-    document.getElementById("sidebar").classList.add("close");
-    document.getElementById("menuOpen").style.display = "block";
-    document.getElementById("menuClose").style.display = "none";
-
-    const mediaQuery = window.matchMedia('(max-width: 400px)')
-    if (mediaQuery.matches) {
-      const boxes = document.querySelectorAll('.sectors');
-      for (const sectors of boxes) {
-        sectors.style.width = "calc(100% - 78px)";
-      }
-      const flexo = document.querySelectorAll('.atract');
-      for (const atract of flexo) {
-        atract.style.display = "none";
-      }
-      const widthi = document.querySelectorAll('.sectors');
-      for (const sectors of widthi) {
-        sectors.style.width = "100%";
-      }
-    }
+    document.getElementById("sidebar").classList.add("transform");
   }
 },
   false
