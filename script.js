90f1a114abc55a4224f4578f2d63fd7e7758034b
Hassan El-Deghedy <106387607+LoLoch0@users.noreply.github.com>
2022-05-28 03:42:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,18 @@ document.addEventListener("click", function(event) {
       document.getElementById("sidebar").classList.remove("close");
       document.getElementById("menuOpen").style.display = "none";
       document.getElementById("menuClose").style.display = "block";
-      const boxes = document.querySelectorAll('.sectors');
-      for (const sectors of boxes) {
-        sectors.style.width = "calc(100% - 260px)";
-      }
-      const flexo = document.querySelectorAll('.atract');
-      for (const atract of flexo) {
-        atract.style.display = "flex";
-      }
+
+      
       const mediaQuery = window.matchMedia('(max-width: 400px)')
       if (mediaQuery.matches) {
+        const boxes = document.querySelectorAll('.sectors');
+        for (const sectors of boxes) {
+          sectors.style.width = "calc(100% - 260px)";
+        }
+        const flexo = document.querySelectorAll('.atract');
+        for (const atract of flexo) {
+          atract.style.display = "flex";
+        }
         const widthi = document.querySelectorAll('.sectors');
         for (const sectors of widthi) {
           sectors.style.width = "calc(100% - 78px)";
@@ -30,16 +32,17 @@ document.addEventListener("click", function(event) {
     document.getElementById("sidebar").classList.add("close");
     document.getElementById("menuOpen").style.display = "block";
     document.getElementById("menuClose").style.display = "none";
-    const boxes = document.querySelectorAll('.sectors');
-    for (const sectors of boxes) {
-      sectors.style.width = "calc(100% - 78px)";
-    }
-    const flexo = document.querySelectorAll('.atract');
-    for (const atract of flexo) {
-      atract.style.display = "none";
-    }
+
     const mediaQuery = window.matchMedia('(max-width: 400px)')
     if (mediaQuery.matches) {
+      const boxes = document.querySelectorAll('.sectors');
+      for (const sectors of boxes) {
+        sectors.style.width = "calc(100% - 78px)";
+      }
+      const flexo = document.querySelectorAll('.atract');
+      for (const atract of flexo) {
+        atract.style.display = "none";
+      }
       const widthi = document.querySelectorAll('.sectors');
       for (const sectors of widthi) {
         sectors.style.width = "100%";
